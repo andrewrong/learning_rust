bc93da4cd779e0ddbc867eed3a8d4ad9a6c84abf
shenmengye <shenmengye@qiniu.com>
2021-07-22 09:50:28 +0800

    commit

---
+++ learning/src/compound.rs
@@ -0,0 +1,116 @@
+
+#[cfg(test)]
+mod tests {
+    use std::path::PrefixComponent;
+
+    #[test]
+    fn test_vector() {
+        let mut vi32 :Vec<i32> = Vec::new();
+        //insert
+        vi32.push(1);
+        vi32.push(2);
+        vi32.push(3);
+
+        println!("{:?}", vi32);
+
+        //pop
+        let pop_value = vi32.pop();
+        if let Some(value) = pop_value {
+            println!("{:?}", value);
+        }
+
+        //get / []
+        let index1 = &vi32[0];
+        println!("{:?}", index1);
+
+        let index2 = vi32.get(0);
+        if let  Some(value) = index2 {
+            println!("{:?}", value);
+        }
+
+        let idx3 = vi32.get(4);
+        if let  Some(value) = idx3 {
+            println!("{:?}", value);
+        } else {
+            println!("none");
+        }
+    }
+
+    #[test]
+    fn test_vector_borrow() {
+        let mut v1 = vec![1,2,3];
+        //  a1 会占用不可变的借用，v1.push 就不能用，因为会修改到 a1 的借用
+        // let a1 = &v1[0];
+        v1.push(5);
+
+        // println!("{:?}", a1);
+    }
+
+    #[test]
+    fn test_vector_for() {
+        let mut v1 = vec![1,2,3];
+        for ele in &v1 {
+            println!("{:?}", ele);
+        }
+        
+        // 迭代器; 会不会影响 ownership
+        for ele in &mut v1 {
+            *ele += 5;
+        }
+        println!("{:?}", v1);
+    }
+
+    #[test]
+    fn test_vector_ownership() {
+        let mut v1 = vec![String::from("hello"), String::from("world")];
+
+        /**
+         * 迭代器有 3 种，三种分别
+         * 1. 返回 immutable reference
+         * 2. 返回 mutable reference
+         * 3. 返回 对应的 ownership，已经是 move 语意了
+         */
+        for ele in &v1 {
+            println!("{:?}", ele);
+        }
+
+        for ele in & mut v1 {
+            println!("{:?}", ele);
+        }
+
+        for ele in v1.into_iter() {
+            println!("{:?}", ele);
+        }
+
+        // println!("{:?}", v1);
+    }
+
+    #[test]
+    fn test_iter() {
+        //iter 其实本质是一个状态机器，它并没有所有的计算结果保存起来，只有你在调用 next 的时候才驱动它
+        let mut iter = (1..10).filter(|x| x % 2 == 0);
+        while let Some(x) = iter.next() { 
+            println!("x:{:?}", x);
+        }
+    }
+
+    #[test]
+    fn test_string() {
+        let a = "hello";
+        let strA = a.to_string();
+
+        println!("{:?}", strA);
+    }
+
+    #[test]
+    fn test_add_string() {
+        let a = "hello".to_string();
+        let b = "world".to_string();
+
+        let c = a + &b;
+        // println!("{:?}", a); // error, a have moved
+        println!("{:?}", c);
+
+        println!("{:?}", format!("{}-{}", c, b));
+    }
+}
--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,5 +1,6 @@
 mod ownership;
 mod option;
+mod compound;
 fn main() {
     println!("Hello, world!");
 }
